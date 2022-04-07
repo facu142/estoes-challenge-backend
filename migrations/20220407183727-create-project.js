7b6b96eefba25b1279e7dc33dbbe01ec92bcea5d
Facundo Montenegro <facumonte29@gmail.com>
2022-04-07 16:36:41 -0300

    faltan la asociacion many to many

---
+++ migrations/20220407183727-create-project.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Projects', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      status: {
+        type: Sequelize.BOOLEAN,
+      },
+      projectManagerId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Projects');
+  }
+};
---
+++ seeders/20220407185628-create-projects.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    await queryInterface.bulkInsert('projects', [
+      {
+        name: 'Project 1',
+        description: 'Project 1 description',
+        status: true,
+        projectManagerId: 1,
+        assignedTo: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Project 2',
+        description: 'Project 2 description',
+        status: true,
+        projectManagerId: 1,
+        assignedTo: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ], {});
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('', null, {});
+     */
+  }
+};
